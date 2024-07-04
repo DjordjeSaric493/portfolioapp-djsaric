da47cacaacda24a97215efc9b2dd78f1b328b0df
DjordjeSaric493 <djordjesaric1999@gmail.com>
2024-07-04 21:32:36 +0200

    svg fix

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,52 +1,50 @@
-import 'package:flutter/material.dart';
 import 'package:device_frame/device_frame.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:portfolioapp_test/utils/const.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    //MediaQuery.of(context).size retrieves the size of the screen, Size is a class that holds the width and height of the screen
+    Size size = MediaQuery.of(context).size; //size of device to get responsive
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    //The argument type '(MaterialColor, MaterialColor)' can't be assigned to the parameter type 'List<Color>'.
-                    //to avoid this instead of colors: (Colors.blue,Colors.purple) use [] brackets bacuse it's a list
-                    colors: [
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              //that nice looking linear gradient in the background
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                colors: [
                   Colors.blue,
-                  Colors.purple
-                ] //fixed, didn't take long to figure out
-                    )),
-          ),
-          SvgPicture.asset(
-            "assets/assets/images/Hexagon.svg",
-            height: size.height,
-            fit: BoxFit.cover,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //that nice looking simulating device on my page
-              DeviceFrame(
-                device: Devices.android.samsungGalaxyS20,
-                screen: const Center(
-                  child: Text(
-                    'Jel radi ovo?',
-                    style: TextStyle(color: Colors.white, fontSize: 50),
+                  Colors.black45
+                ], //[] it's a list '(MaterialColor, Color)' can't be assigned to a parameter of type 'List<Color>'
+              )),
+            ),
+            SvgPicture.asset(
+              "assets/images/cloudySea.svg",
+              height: size.height,
+              fit: BoxFit.cover,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //that nice looking samsung S something
+                DeviceFrame(
+                  device: Devices.android.samsungGalaxyS20,
+                  screen: const Center(
+                    child: Text(
+                      "Jel radi?",
+                      style: TextStyle(color: Colors.white, fontSize: 50),
+                    ),
                   ),
-                ),
-              )
-            ],
-          ),
-        ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/utils/const.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-const hexa_svg = 'assets/images/Hexagon.svg';
